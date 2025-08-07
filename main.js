b86a8fb0d55e1bd885a8e821c394e8e905dd348d
EDEM-odg <amenakporjudecartel@gmail.com>
2025-08-07 22:32:30 +0000

    New message

--- main.js
+++ main.js
@@ -3,17 +3,20 @@ function Person(firstName, lastName, dod) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.dob = new Date(dob);
-    this.getBirthYear = function() {
-        return this.dob.getFullYear();
-    }
-    this.getFullName = function() {
-        return `${this.firstName} ${this.lastName}`;
-    }
 }
+
+Person.prototype.getBirthYear = function() {
+    return this.dob.getFullYear();
+}
+
+Person.prototype.getFullName = function() {
+    return `${this.firstName} ${this.lastName}`;
+}
+
 
 // Instantiate object
 const person1 = new Person('John', 'Doe', '4-3-1980');
 const person2 = new Person('Mary', 'Smith', '3-3-1980')
 
-console.log(person1.getBirthYear());
-console.log(person2.getFullName());
+console.log(person1);
+console.log(person2.getBirthYear());
