e79cf00e7415f98fc14c91edaec671c49b39d6f6
EDEM-odg <amenakporjudecartel@gmail.com>
2025-08-07 20:26:05 +0000

    Added new  message

--- main.js
+++ main.js
@@ -1,26 +1,5 @@
-const todos = [
-    {
-        id: 1,
-        text: 'Take out trash',
-        isCompleted: true
-    },
-    {
-        id: 2,
-        text: 'Meeting with boss',
-        isCompleted: true
-    },
-    {
-        id: 3,
-        text: 'Dentist appointment',
-        isCompleted: false
-    }
-]
+const x = 4;
+const y = 10;
 
-// forEach, map, filter
-const todoCompleted = todos.filter(function(todo) {
-    return todo.isCompleted === true;
-}).map(function(todo) { 
-    return todo.text;
-})
-
-console.log(todoCompleted); 
+if(x > 5 || y > 10) {
+    console.log("x is 10");
